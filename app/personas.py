3ad185d635c011723f4d64cda9b47559c9268789
Lalivick71 <vikitoria7168@gmail.com>
2022-11-29 14:30:52 -0300

    primer proyecto

---
+++ app/personas.py
@@ -0,0 +1,42 @@
+import datetime
+
+from cuenta import Cuenta
+def convertir_fecha(string_fecha):
+    anio = string_fecha[0:4]
+    mes = string_fecha[5:7]
+    dia = string_fecha[8:10]
+    return datetime.date(int(anio),int(mes),int(dia))
+
+class persona(object):
+    def __init__(self,dni,nombre,str_fecha_nacimiento):
+        self.nombre = nombre
+        self.fecha_nacimiento = convertir_fecha(str_fecha_nacimiento)
+        self.dni = dni
+        self.cuentas = []
+
+
+    def __str__(self):
+        return f"Nombre:{self.nombre}"
+
+    def saludo(self):    
+        return f"hola {self.nombre}"
+
+    def crear_cuenta(self):
+
+        cuenta = Cuenta()
+        cuenta.crear_movimiento("| Cuenta creada.se otorgaron de bienvenida $", 1500)
+        self.cuentas.append(cuenta)
+
+    def obtener_todos_los_movimientos(self):
+        todos_los_movimientos = []
+        for cuenta in self.cuentas:
+            todos_los_movimientos += cuenta.movimientos
+            return todos_los_movimientosss
+
+    
+
+
+        
+
+
+
---
+++ app/proceso_cuentas.py
@@ -0,0 +1,22 @@
+import csv
+
+from persona import Persona
+
+def crear_cuentas():
+
+    personas = {}
+    archivo = open("personas.csv", "r")
+
+    archivo_csv = csv.reader(archivo)
+    next(archivo_csv)
+    for nombre,dni,fecha_nacimiento in archivo_csv:
+        persona.crear_cuenta()
+
+        personas["dni"] = persona
+
+    archivo.close()
+    print (f" {personas}")
+    return personas
+
+
+
